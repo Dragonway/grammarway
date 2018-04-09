3b7237a4add4cd824d1f05188a1b3b552652d8b7
Ivan Leonov <ivan.leonov.d@yandex.com>
2018-04-09 22:43:09 +0300

    Add some initial stub classes

---
+++ grammarway.py
@@ -0,0 +1,29 @@
+class Node:
+    """Doc stub"""
+
+    def __init__(self):
+        pass
+
+
+class Lexeme(Node):
+    """Doc stub"""
+
+    def __init__(self):
+        super().__init__()
+
+    def parse(self):
+        pass
+
+
+class Empty(Lexeme):
+    """Doc stub"""
+
+    def __init__(self):
+        super().__init__()
+
+
+class Literal(Lexeme):
+    """Doc stub"""
+
+    def __init__(self):
+        super().__init__()

204a99087b042c22565a4ac43fca94e490815b93
Ivan Leonov <ivan.leonov.d@yandex.com>
2018-04-18 23:26:06 +0300

    Add initial stub for tests

---
+++ test_grammarway.py
@@ -0,0 +1,22 @@
+import unittest
+import grammarway as gw
+
+class TestLexemes(unittest.TestCase):
+
+    def test_empty(self):
+        empty = gw.Empty()
+
+        self.assertTrue(empty.parse("grammarway"))
+        self.assertTrue(empty.parse(" grammarway"))
+        self.assertTrue(empty.parse(""))
+
+    def test_literal(self):
+        literal = gw.Literal("grammarway")
+
+        self.assertTrue(literal.parse("grammarway"))
+        self.assertTrue(literal.parse("grammarways"))
+        self.assertFalse(literal.parse("grammarwei"))
+        self.assertFalse(literal.parse("drammarway"))
+
+if __name__ == '__main__':
+    unittest.main()
